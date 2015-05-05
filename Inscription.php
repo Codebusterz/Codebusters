3a331516ceaafd5d78627bdb79887b85ccd6b8e9
Marsou77 <qzmiro@gmail.com>
2015-05-05 21:17:01 +0200

    réorganisation

--- Inscription.php
+++ Inscription.php
@@ -1,18 +1,19 @@
 <?php
 
-	if (isset($_POST["valider"])) {
-	$nom = $_POST["nom"];
-	$prenom = $_POST["prenom"];
-	$pseudo= $_POST["pseudo"];
-	$email = $_POST["email"];
-	$mdp = sha1($_POST["mdp"]);
-	$age = $_POST["age"];
-
 	$bdd = mysqli_connect("localhost","root","","[projet]lolgames");
-	$query = " INSERT INTO users (nom, prenom, pseudo, mdp, email, age) VALUES ('".$nom."','".$prenom."','".$pseudo."','".$mdp."','".$email."','".$age."'); ";
-	$res = mysqli_query($bdd, $query) or die(mysql_error($bdd));
 
-}
+	if (isset($_POST["inscription"]))
+	{
+		$nom = $_POST["nom"];
+		$prenom = $_POST["prenom"];
+		$pseudo= $_POST["pseudo"];
+		$email = $_POST["email"];
+		$mdp = sha1($_POST["mdp"]);
+		$age = $_POST["age"];
+
+		$query = "INSERT INTO users (nom, prenom, pseudo, mdp, email, age) VALUES ('".$nom."','".$prenom."','".$pseudo."','".$mdp."','".$email."','".$age."'); ";
+		$res = mysqli_query($bdd, $query) or die(mysql_error($bdd));
+	}
 
 	include_once("inscription.html");
 ?>
